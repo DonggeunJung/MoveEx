d25c050f326e750db5b867c984c117dbe3337503
DonggeunJung <topsan72@gmail.com>
2018-07-20 09:00:40 -0400

    Initial commit

---
+++ Assets/MoveObject.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObject : MonoBehaviour {
+	public float jumpHeight = 3;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 moveValue = transform.position;
+		if( Input.GetButtonUp("Jump") ) {
+			moveValue.y += jumpHeight;
+		}
+		moveValue.z += Input.GetAxis ("Vertical") / 2;
+		transform.position = moveValue;
+
+		transform.Rotate(0, Input.GetAxis ("Horizontal") * 3, 0);
+
+		if (Input.GetMouseButtonUp(0))
+			transform.Translate (0f, 0f, 0.3f);
+		if (Input.GetMouseButtonUp(1))
+			transform.Translate (0f, 0f, -0.3f);
+	}
+}
